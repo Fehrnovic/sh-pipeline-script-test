690ac0b3124d97daf335512992978f6cafcce989
Oscar Fehrn <33087243+Fehrnovic@users.noreply.github.com>
2020-11-20 15:05:56 +0100

    Update half-script.sh

--- half-script.sh
+++ half-script.sh
@@ -1,1 +1,14 @@
+# Install docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
+# Install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install to login to docker
+i.	sudo apt install gnupg2 pass -y
